499094f1578f19ac4ca07fa98c8d65831e2ae1c6
RyuichiroYoshida <ysdr1120@icloud.com>
2025-06-07 01:09:31 +0900

    user.goの修正: AvatarフィールドをUser構造体に追加

--- backend/models/user.go
+++ backend/models/user.go
@@ -6,6 +6,7 @@ type User struct {
 	gorm.Model
 	Id        string `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
 	Name      string `gorm:"not null"`
+	Avatar    string `gorm:"not null"`
 	DiscordId string `gorm:"not null;unique"`
 	CreatedAt string `gorm:"not null"`
 }
