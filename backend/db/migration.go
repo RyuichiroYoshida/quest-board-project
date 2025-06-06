e870b8634c14fa98683b28760768ef2503b38053
RyuichiroYoshida <ysdr1120@icloud.com>
2025-06-06 19:02:50 +0900

    データベース接続とマイグレーション機能を追加

---
+++ backend/db/migration.go
@@ -0,0 +1,48 @@
+package db
+
+import (
+	"os"
+
+	"github.com/RyuichiroYoshida/quest-board-project/models"
+	"github.com/RyuichiroYoshida/quest-board-project/utils"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func SetupDb() {
+	// DB接続
+	dsn := getDSN()
+	if dsn == "" {
+		utils.LogError("Failed to get database DSN.")
+		return
+	}
+	// データベース接続
+	database, err := connectDB(dsn)
+	if err != nil {
+		utils.LogError("Failed to connect to the database: %v", err)
+	}
+	// マイグレーション
+	database.AutoMigrate(&models.User{}, &models.Posts{}, &models.Applications{})
+}
+
+func connectDB(dsn string) (*gorm.DB, error) {
+	var err error
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func getDSN() string {
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	port := os.Getenv("DB_PORT")
+	dbname := os.Getenv("DB_NAME")
+	if host == "" || user == "" || password == "" || port == "" || dbname == "" {
+		utils.LogError("Database environment variables are not set properly.")
+		return ""
+	}
+	return "host=" + host + " user=" + user + " password=" + password + " port=" + port + " dbname=" + dbname + " sslmode=disable TimeZone=Asia/Tokyo"
+}
