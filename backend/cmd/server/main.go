2e4cc3875331ae4647a21bbd017d1b2696601e2a
RyuichiroYoshida <ysdr1120@icloud.com>
2025-06-08 11:49:40 +0900

    SetupDbの戻り値を変数に格納し、DIコンテナの初期化を追加

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"github.com/RyuichiroYoshida/quest-board-project/db"
+	"github.com/RyuichiroYoshida/quest-board-project/di"
 	"github.com/RyuichiroYoshida/quest-board-project/routes"
 	"github.com/RyuichiroYoshida/quest-board-project/utils"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	db.SetupDb()
+	database := db.SetupDb()
+
+	container := di.InitContainer(database)
 
 	router := gin.Default()
-	routes.Setup(router)
+	routes.Setup(router, container)
 
 	if err := router.Run(":8080"); err != nil {
 		utils.LogError("Failed to start server", err)
