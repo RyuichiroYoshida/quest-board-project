f3fb49ed1494f05470923b6a41f2b8a07ea1e5f4
RyuichiroYoshida <ysdr1120@icloud.com>
2025-06-08 11:49:00 +0900

    router.goの追加: APIルーティングのセットアップを実装

---
+++ backend/routes/router.go
@@ -0,0 +1,21 @@
+package routes
+
+import (
+	"log"
+
+	"github.com/RyuichiroYoshida/quest-board-project/di"
+	"github.com/gin-gonic/gin"
+)
+
+func Setup(r *gin.Engine, c *di.Container) {
+	authH := c.AuthHandler
+	api := r.Group("/api")
+	{
+		auth := api.Group("/auth")
+		{
+			auth.GET("/login/discord", authH.LoginDiscord)
+			auth.GET("/exchange", authH.ExchangeCode)
+		}
+	}
+	log.Println(api)
+}
